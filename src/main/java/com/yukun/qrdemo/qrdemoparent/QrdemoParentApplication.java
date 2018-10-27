1a0545d4541e8befa9f2bcc691baf853a508e661
Yukun Wen <530016380@qq.com>
2018-10-27 10:02:44 +0800

    项目初始化

---
+++ src/main/java/com/yukun/qrdemo/qrdemoparent/QrdemoParentApplication.java
@@ -0,0 +1,12 @@
+package com.yukun.qrdemo.qrdemoparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrdemoParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrdemoParentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yukun/qrdemo/qrdemoparent/QrdemoParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yukun.qrdemo.qrdemoparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QrdemoParentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
